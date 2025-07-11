d7ddf5f6b997fd10f60dae0272eb18138a230705
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-11 18:47:37 +0900

    [FIX] Tab bar 레이아웃 수정

--- layouts/tab-bar.tsx
+++ layouts/tab-bar.tsx
@@ -1,19 +1,23 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function TabBar() {
-    const LinkClassName = 'flex flex-col items-center gap-px'
+    const pathname = usePathname()
+    const LinkClassName = 'flex flex-col items-center gap-px w-full'
+    const borderClassName = 'border-t-2 border-t-black-500'
     return (
-            <div className="flex items-center justify-center gap-2 w-full" style={{borderTop:'2px solid red'}}>
-                <Link href="/" className={LinkClassName}>
+            <div className="flex items-center justify-between w-full">
+                <Link href="/" className={`${LinkClassName} ${pathname === '/' ? borderClassName : ''}`}>
                     <span>지도 보기</span>
                 </Link>
-                <Link href="/calendar" className={LinkClassName}>
+                <Link href="/calendar" className={`${LinkClassName} ${pathname === '/calendar' ? borderClassName : ''}`}>
                     <span>달력 보기</span>
                 </Link>
-                <Link href="/month-festival" className={LinkClassName}>
+                <Link href="/month-festival" className={`${LinkClassName} ${pathname === '/month-festival' ? borderClassName : ''}`}>
                     <span>이달의 축제</span>
                 </Link>
-                <Link href="/customized" className={LinkClassName}>
+                <Link href="/customized" className={`${LinkClassName} ${pathname === '/customized' ? borderClassName : ''}`}>
                     <span>맞춤 축제</span>
                 </Link>
             </div>
