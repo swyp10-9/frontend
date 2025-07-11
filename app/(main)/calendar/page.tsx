23d53a97433830f4afd9f667dc7269723b15d3ad
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-11 17:48:04 +0900

    [FEAT] 달력 페이지 임시 세팅

--- app/(main)/calendar/page.tsx
+++ app/(main)/calendar/page.tsx
@@ -1,7 +1,10 @@
+import Calendar from "@/components/Calendar";
+
 export default function CalendarPage() {
     return (
         <div>
             <h1>달력 보기</h1>
+            <Calendar/>
         </div>
     )
 }
