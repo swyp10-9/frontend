e260c585b7a8d47971dcb7d096d2a2557a76ad72
Seongbin Kim <seongbin9786@gmail.com>
2025-07-19 18:34:06 +0900

    fix: 기존 빌드 오류 수정

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(_req: NextRequest, _res: NextResponse) {
+export async function GET(_req: NextRequest) {
   const cookieStore = cookies();
   const cookieString = (await cookieStore).toString();
   console.log('cookieString:::', cookieString);
