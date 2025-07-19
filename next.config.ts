e3b00e9de6e3f458fb172f14e01fbd4941384ba5
Seongbin Kim <seongbin9786@gmail.com>
2025-07-19 14:44:27 +0900

    chore: Prettier, ESLint 설치 및 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // NOTE: 빌드 이전 직접 lint 실행
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
