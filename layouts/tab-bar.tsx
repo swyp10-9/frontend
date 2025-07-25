7015a770892f1ce35dda7a8633988ca0739e6113
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-25 16:34:52 +0900

    feat: 네비게이션바

--- layouts/tab-bar.tsx
+++ layouts/tab-bar.tsx
@@ -1,25 +1,91 @@
-'use client'
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+'use client';
+
+import { Icon } from '@iconify/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function TabBar() {
-    const pathname = usePathname()
-    const LinkClassName = 'flex flex-col items-center gap-px w-full'
-    const borderClassName = 'border-t-2 border-t-black-500'
-    return (
-            <div className="flex items-center justify-between w-full">
-                <Link href="/" className={`${LinkClassName} ${pathname === '/' ? borderClassName : ''}`}>
-                    <span>지도 보기</span>
-                </Link>
-                <Link href="/calendar" className={`${LinkClassName} ${pathname === '/calendar' ? borderClassName : ''}`}>
-                    <span>달력 보기</span>
-                </Link>
-                <Link href="/month-festival" className={`${LinkClassName} ${pathname === '/month-festival' ? borderClassName : ''}`}>
-                    <span>이달의 축제</span>
-                </Link>
-                <Link href="/customized" className={`${LinkClassName} ${pathname === '/customized' ? borderClassName : ''}`}>
-                    <span>맞춤 축제</span>
-                </Link>
-            </div>
-    )
+  const pathname = usePathname();
+  const LinkClassName = 'flex flex-col items-center gap-px w-full';
+
+  return (
+    <div className='border-t-gray-400 border-t-1 py-3 box-border'>
+      <div className='flex items-center justify-between w-full'>
+        <Link
+          href='/'
+          className={`${LinkClassName} ${pathname === '/' ? '' : ''}`}
+        >
+          <Icon
+            className='mb-0.5'
+            icon={pathname === '/' ? 'heroicons:map-solid' : 'heroicons:map'}
+            fontSize={24}
+            color={pathname === '/' ? undefined : 'gray'}
+          />
+          <span
+            className={`${pathname === '/' ? 'text-sub-head-3' : 'text-caption'}`}
+          >
+            지도 보기
+          </span>
+        </Link>
+        <Link
+          href='/calendar'
+          className={`${LinkClassName} ${pathname === '/calendar' ? '' : ''}`}
+        >
+          <Icon
+            className='mb-0.5'
+            icon={
+              pathname === '/calendar' ? 'uis:calendar' : 'proicons:calendar'
+            }
+            fontSize={24}
+            color={pathname === '/calendar' ? undefined : 'gray'}
+          />
+          <span
+            className={`${pathname === '/calendar' ? 'text-sub-head-3' : 'text-caption'}`}
+          >
+            달력 보기
+          </span>
+        </Link>
+        <Link
+          href='/month-festival'
+          className={`${LinkClassName} ${pathname === '/month-festival' ? '' : ''}`}
+        >
+          <Icon
+            className='mb-0.5'
+            icon={
+              pathname === '/month-festival'
+                ? 'bxs:calendar-star'
+                : 'bx:calendar-star'
+            }
+            fontSize={24}
+            color={pathname === '/month-festival' ? undefined : 'gray'}
+          />
+          <span
+            className={`${pathname === '/month-festival' ? 'text-sub-head-3' : 'text-caption'}`}
+          >
+            이달의 축제
+          </span>
+        </Link>
+        <Link
+          href='/customized'
+          className={`${LinkClassName} ${pathname === '/customized' ? '' : ''}`}
+        >
+          <Icon
+            className='mb-0.5'
+            icon={
+              pathname === '/customized'
+                ? 'mingcute:target-fill'
+                : 'mingcute:target-line'
+            }
+            fontSize={24}
+            color={pathname === '/customized' ? undefined : 'gray'}
+          />
+          <span
+            className={`${pathname === '/customized' ? 'text-sub-head-3' : 'text-caption'}`}
+          >
+            맞춤 축제
+          </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
