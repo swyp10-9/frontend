239b80341aff2daeedc75d3354607e74d1190925
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-25 19:18:04 +0900

    feat: 로그인페이지 1차 마크업

--- app/(auth)/login/login-button.tsx
+++ app/(auth)/login/login-button.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+
 import config from '@/config';
 
 export default function LoginButton({
@@ -13,8 +15,24 @@ export default function LoginButton({
 
   return (
     <div>
-      {type === 'not-login' && <a>로그인 없이 볼게요</a>}
-      {type === 'kakao' && <a href={kakaoUrl}>10초만에 카카오로 시작하기</a>}
+      {type === 'not-login' && (
+        <a className=''>
+          <div className='flex justify-center items-center gap-2.5 rounded-lg w-[320px] h-[58px] border-gray-50 border'>
+            <p className='text-head'>로그인 없이 볼게요</p>
+          </div>
+        </a>
+      )}
+      {type === 'kakao' && (
+        <a href={kakaoUrl}>
+          <div
+            className='flex justify-center items-center gap-2.5 rounded-lg px-5 py-4.5'
+            style={{ backgroundColor: '#FEE500' }}
+          >
+            <Image src='/sns/kakao.png' alt='kakao' width={24} height={24} />
+            <p className='text-head'>10초만에 카카오로 시작하기</p>
+          </div>
+        </a>
+      )}
       {type === 'naver' && <a>10초만에 네이버로 시작하기</a>}
     </div>
   );
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,13 +2,21 @@ import LoginButton from './login-button';
 
 export default function Login() {
   return (
-    <div className='h-screen border flex flex-col w-full items-center justify-center'>
-      <h1>축지법</h1>
-      <h4 className='text-center'>
-        간편하게 로그인하고 <br /> 다양한 축제를 둘러보세요.
-      </h4>
-      <LoginButton type='kakao' />
-      <LoginButton type='not-login' />
+    <div className='h-screen relative flex flex-col w-full gap-10 items-center justify-center'>
+      <div className='flex flex-col items-center justify-center gap-3'>
+        <p className='text-head-2'>축지법</p>
+        <p className='text-center text-body-2'>
+          간편하게 로그인하고 <br /> 다양한 축제를 둘러보세요.
+        </p>
+      </div>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <LoginButton type='kakao' />
+        <LoginButton type='not-login' />
+        <p className='text-body-2'>일부 기능이 제한됩니다.</p>
+      </div>
+      <p className='text-caption text-gray-400 fixed bottom-10 text-center'>
+        가입 시 개인정보처리방침 및 이용약관에 동의하는 것으로 간주됩니다.
+      </p>
     </div>
   );
 }
