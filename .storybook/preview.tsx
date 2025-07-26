29be26d74951e34256f1569fae8d34fca60bdb44
Seongbin Kim <seongbin9786@gmail.com>
2025-07-26 13:43:16 +0900

    chore: Storybook에 TailwindCSS import

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import type { Preview } from '@storybook/nextjs-vite';
 import { mswLoader } from 'msw-storybook-addon';
 
+// Tailwind CSS 스타일 import
+import '@/app/globals.css';
 import { ReactQueryClientProvider } from '@/app/react-query-provider';
 import { initializeMSWOnStorybook } from '@/configs/msw/initializeMSWOnStorybook';
 
