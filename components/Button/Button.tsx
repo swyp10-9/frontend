15189327cf9c8eb5359be1ef7b2f5af31f5198a9
Seongbin Kim <seongbin9786@gmail.com>
2025-07-26 22:17:16 +0900

    feat: CJB-11 공통 버튼 컴포넌트 추가

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,40 +6,39 @@ import { type VariantProps, cva } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "flex cursor-pointer items-center justify-center gap-2 whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+        primary:
+          'border-red bg-gray-700 text-white active:bg-gray-400 disabled:bg-gray-50 disabled:text-gray-200',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-        ghost:
-          'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+          'border border-gray-100 bg-white text-gray-700 disabled:border-gray-100 disabled:bg-gray-50 disabled:text-gray-200',
+        error: 'bg-red text-white',
+        ghost: 'bg-transparent text-black',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        xs: 'w-auto gap-0.5 px-2 py-1 ui-text-sub-head-3', // NOTE: xs, sm은 너비, 높이가 정해져 있지 않음
+        sm: 'w-auto gap-1 px-3 py-2 ui-text-sub-head-2',
+        md: 'box-border h-13 w-full ui-text-sub-head',
+        lg: 'box-border h-14 w-full ui-text-sub-head',
+      },
+      rounded: {
+        default: 'rounded-[8px]',
+        full: 'rounded-full',
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      rounded: 'default',
     },
   },
 );
 
-function Button({
+export function Button({
   className,
   variant,
   size,
+  rounded,
   asChild = false,
   ...props
 }: React.ComponentProps<'button'> &
@@ -51,10 +50,8 @@ function Button({
   return (
     <Comp
       data-slot='button'
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, rounded, className }))}
       {...props}
     />
   );
 }
-
-export { Button, buttonVariants };
