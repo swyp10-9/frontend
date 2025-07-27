2c8429d8b2a37d02b5a9499fbdfa88523e3f2aee
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-27 17:02:31 +0900

    chore: searchParams 타입 변경

--- app/(main)/calendar/page.tsx
+++ app/(main)/calendar/page.tsx
@@ -5,14 +5,14 @@ import List from './_modules/list';
 export default async function CalendarPage({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+  searchParams: { selected: string };
 }) {
   const { selected } = await searchParams;
 
   return (
     <div className='flex flex-col gap-5'>
-      <Calendar initialDate={new Date(selected as string)} />
-      <List selected={selected as string} />
+      <Calendar initialDate={new Date(selected)} />
+      <List selected={selected} />
     </div>
   );
 }
