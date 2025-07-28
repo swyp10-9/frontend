998d04de173676e4b5684dff0edd46cb6cf00788
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-28 21:08:30 +0900

    wip: 북마크 작업중

--- app/mypage/_modules/bookmark-list.tsx
+++ app/mypage/_modules/bookmark-list.tsx
@@ -1,3 +1,20 @@
+import FestivalListView from '@/components/festival-list-view';
+
 export default function BookmarkList() {
-  return <div>BookmarkList</div>;
+  return (
+    <div className='flex flex-col gap-5 overflow-y-auto py-5'>
+      {Array.from({ length: 20 }).map((_, index) => (
+        <FestivalListView
+          key={index}
+          image={'https://picsum.photos/200/300'}
+          theme='music_performance'
+          title='북마크'
+          loc='서울'
+          end_date='2025.01.01'
+          start_date='2025.01.01'
+          is_marked={true}
+        />
+      ))}
+    </div>
+  );
 }
