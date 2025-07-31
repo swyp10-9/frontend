ab08d61f26e7abaa0579a371857337b311e7972e
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-07-31 19:10:20 +0900

    fix: 파일명 edit 오타 수정

--- app/mypage/edit/page.tsx
+++ app/mypage/edit/page.tsx
@@ -1,5 +1,5 @@
 // 'use client';
-import EditClient from './_modules/eidt-client';
+import EditClient from './_modules/edit-client';
 
 export default function MyPageEdit() {
   return <EditClient />;
