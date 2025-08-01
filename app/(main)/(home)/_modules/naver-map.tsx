4373cc63122d636b7ea8f5ed5abc8b1826b3a90a
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-08-01 15:19:26 +0900

    fix: 마커 마크업 조정

--- app/(main)/(home)/_modules/naver-map.tsx
+++ app/(main)/(home)/_modules/naver-map.tsx
@@ -243,16 +243,16 @@ export default function NaverMap({
         markerElement.innerHTML = `
           <a href="/festival/${festival.id}">
             <div class="box-border flex h-[64px] w-[224px] items-center gap-2 rounded-lg bg-white py-2 pr-3 pl-2" style="box-shadow: 0 0 5px 0 rgba(0,0,0,0.18);">
-            <img src="${festival.image}" alt="${festival.title}" width="48" height="48" class="aspect-square rounded-sm object-cover" />
+            <img src="${festival?.image}" alt="${festival?.title || ''}" width="48" height="48" class="aspect-square rounded-sm object-cover" />
             <div class="flex w-full flex-col justify-center gap-1">
               <div class="flex w-full items-center justify-between">
                 <div class="flex h-[22px] w-[54px] items-center justify-center rounded-sm" style="background-color: ${theme.bgColor}; color: ${theme.color};">
-                  <p class="ui-text-sub-head-3">${theme.label}</p>
+                  <p class="ui-text-sub-head-3">${theme?.label || ''}</p>
                 </div>
                 ${festival?.is_marked ? marked : unMarked}
               </div>
                 <div class="flex items-center">
-                  <p class="line-clamp-1 ui-text-head-2">${festival.title}</p>
+                  <p class="line-clamp-1 ui-text-sub-head max-w-[135px]">${festival?.title || ''}</p>
                   ${rightChevron}
                 </div>
               </div>
