22c7c7743a788a0bf202a50c6e90d7708750ea86
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-08-08 00:24:52 +0900

    fix: 이달의 축제 탭 주석처리 및 box shadow 제거

--- layouts/tab-bar.tsx
+++ layouts/tab-bar.tsx
@@ -9,7 +9,7 @@ export default function TabBar() {
   const LinkClassName = 'flex flex-col items-center gap-px flex-1';
 
   return (
-    <div className='w-full border-t border-gray-400 bg-white px-5 py-3 shadow-lg'>
+    <div className='w-full border-t border-gray-400 bg-white px-5 py-3'>
       <div className='flex w-full items-center justify-between'>
         <Link
           href='/'
@@ -45,7 +45,7 @@ export default function TabBar() {
             축제달력
           </span>
         </Link>
-        <Link
+        {/* <Link
           href='/month-festival'
           className={`${LinkClassName} ${pathname === '/month-festival' ? '' : ''}`}
         >
@@ -64,7 +64,7 @@ export default function TabBar() {
           >
             이달의 축제
           </span>
-        </Link>
+        </Link> */}
         {/* <Link
           href='/search'
           className={`${LinkClassName} ${pathname === '/search' ? '' : ''}`}
