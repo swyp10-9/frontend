bcc18b607bf17df39e24a74372058bacc990518b
HyungwonJang0327 <hyungwonjang0327@gmail.com>
2025-08-22 23:54:51 +0900

    feat: 지도 페이지 축제 리스트 컴포넌트 마크업

--- app/(main)/(home)/_modules/map-festival-list.tsx
+++ app/(main)/(home)/_modules/map-festival-list.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import BottomSheet, { BottomSheetRef } from '@/components/bottom-sheet';
+import { Button } from '@/components/Button';
 import FestivalListView from '@/components/festival-list-view';
-import { Drawer, DrawerTrigger } from '@/components/shadcn/drawer';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+} from '@/components/shadcn/drawer';
 
 export default function MapFestivalList() {
-  const bottomListRef = useRef<BottomSheetRef>(null);
   const openRef = useRef<HTMLDivElement>(null);
+  const [activeSnapPoint, setActiveSnapPoint] = useState<
+    number | string | null
+  >(0.5);
 
   useEffect(() => {
     if (openRef?.current) {
@@ -17,34 +23,45 @@ export default function MapFestivalList() {
   }, [openRef]);
 
   return (
-    <Drawer snapPoints={[0.5, 0.9, 1]} modal={false}>
-      <BottomSheet
-        zIndex={1}
-        titleChildren={<></>}
-        removeIndicator
-        ref={bottomListRef}
-      >
-        <div className='z-[1] px-5 pb-[100px]'>
-          <p className='ui-text-head-2'>23개의 축제</p>
-          <div className='box-border flex h-full w-full flex-col items-center justify-center py-5'>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <FestivalListView
-                key={index}
-                image={''}
-                theme={''}
-                title={''}
-                loc={''}
-                start_date={''}
-                end_date={''}
-                is_marked={false}
-                id={0}
-                map_x={''}
-                map_y={''}
-              />
-            ))}
+    <Drawer
+      snapPoints={[0.5, 1]}
+      modal={false}
+      activeSnapPoint={activeSnapPoint}
+      setActiveSnapPoint={setActiveSnapPoint}
+    >
+      <div className='absolute right-4 bottom-4 flex items-center gap-2'>
+        <DrawerTrigger asChild>
+          <Button size={'sm'} variant='primary' rounded='full'>
+            리스트로 보기
+          </Button>
+        </DrawerTrigger>
+      </div>
+      <DrawerContent className='z-[1] h-full'>
+        <div className='flex h-full flex-col px-5 pb-[100px]'>
+          <div className='flex-shrink-0 py-4'>
+            <p className='ui-text-head-2'>23개의 축제</p>
+          </div>
+          <div className='flex-1 overflow-y-auto'>
+            <div className='box-border flex w-full flex-col items-center justify-center py-1'>
+              {Array.from({ length: 15 }).map((_, index) => (
+                <FestivalListView
+                  key={index}
+                  image={''}
+                  theme={''}
+                  title={''}
+                  loc={''}
+                  start_date={''}
+                  end_date={''}
+                  is_marked={false}
+                  id={0}
+                  map_x={''}
+                  map_y={''}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </BottomSheet>
+      </DrawerContent>
       <DrawerTrigger asChild>
         <div ref={openRef} className='hidden' />
       </DrawerTrigger>
